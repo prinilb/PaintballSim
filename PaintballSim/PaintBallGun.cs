27b1e08002eda270b9567f3d1962e555abb58684
prinilb <prinilb@dunwoody.edu>
2022-03-12 22:56:49 -0600

    version without using properties

--- PaintballSim/PaintBallGun.cs
+++ PaintballSim/PaintBallGun.cs
@@ -8,32 +8,34 @@ namespace PaintballSim
 {
     class PaintBallGun
     {
-        public const int MAGAZINE_SIZE = 16;
+        public const int MAGAZINE_SIZE = 16; // public because it needs to be used by Main()
 
         private int balls = 0;
         private int ballsLoaded = 0;
 
-        public int GetBallsLoaded() { return ballsLoaded; }
-        public bool IsEmpty() { return ballsLoaded == 0; }
-        public int GetBalls() { return balls; }
-        public void SetBalls(int numberOfBalls)
+        public int GetBallsLoaded() { return ballsLoaded; } // mini method to find out how many balls are loaded in the gun
+        public bool IsEmpty() { return ballsLoaded == 0; } // mini method for when we run out of ammo (not really neccesary the way we're using the class)
+        public int GetBalls() { return balls; } // find out how much ammo we really got left
+        public void SetBalls(int numberOfBalls) 
         {
             if (numberOfBalls > 0)
                 ballsLoaded = numberOfBalls;
             Reload();
+            /* since the ballsLoaded variable is private (because encapsulation is a good thing), 
+               this is the only way we can actually change that variable */
         }
 
         public void Reload()
         {
             if (ballsLoaded > MAGAZINE_SIZE)
-                ballsLoaded = MAGAZINE_SIZE;
+                ballsLoaded = MAGAZINE_SIZE; // loads gun with a full magazine
             else
-                ballsLoaded = balls;
+                ballsLoaded = balls; // or just fills up with whatever is left for ammo
         }
 
-        public bool Shoot()
+        public bool Shoot() // gotta be able to shoot our gun
         {
-            if (ballsLoaded == 0) return false;
+            if (ballsLoaded == 0) return false; // out of ammo :(
             ballsLoaded--;
             balls--;
             return true;
--- PaintballSim/Program.cs
+++ PaintballSim/Program.cs
@@ -10,6 +10,25 @@ namespace PaintballSim
     {
         static void Main(string[] args)
         {
+            PaintBallGun gun = new PaintBallGun(); 
+
+            while (true)
+            {
+                Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded"); // let em know what they got before they get started
+
+                if (gun.IsEmpty()) Console.WriteLine("WARNING: You're out of ammo"); // a truly tragic event
+
+                Console.WriteLine("Space to shoot, r, to reload, + to add ammo, q to quit");
+                char key = Console.ReadKey(true).KeyChar;
+
+                if (key == ' ')
+                {
+                    Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                }
+                else if (key == 'r') gun.Reload();
+                else if (key == '+') gun.SetBalls(gun.GetBalls() + PaintBallGun.MAGAZINE_SIZE);
+                else if (key == 'q') return;
+            }
         }
     }
 }
